5245df6808e22d1b677c7bb23d6b00b555ab12ba
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-03-26 23:23:35 -0300

    Tratamento de erros

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ import chalk from 'chalk';
 import  fs from  'fs';
 
 
+function trataErro(erro){
+
+   throw new Error(chalk.red(erro.code,'arquivo não encontrado'))
+
+
+}
+
+
+
+
+
 function pegarArquivo(caminhodoArquivo){
    const encoding ='utf-8';
 
 
-   fs.readFile(caminhodoArquivo,encoding,(_,texto) =>{
+   fs.readFile(caminhodoArquivo,encoding,(erro,texto) =>{
+           
+     if(erro){
+
+       trataErro(erro);
+     }
 
      console.log(chalk.green(texto))
 
@@ -18,4 +34,4 @@ function pegarArquivo(caminhodoArquivo){
 
 }
 
-pegarArquivo('./texto.md')
+pegarArquivo('./texto')
