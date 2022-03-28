60adca74a61aa16eb8de1ab49abad06f2af2b861
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-03-27 22:47:38 -0300

    Validando Links

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 import  pegaArquivo from './index.js';
+import  validaURLs from './http-validacao.js';
 import chalk from 'chalk';
 
 
@@ -6,7 +7,11 @@ const caminho = process.argv;
 
 async function processaTexto(caminhoDeArquivo) {
   const resultado = await pegaArquivo(caminhoDeArquivo[2]);
-  console.log(chalk.yellow('lista de links'), resultado);
+  if (caminho[3] === 'validar') {
+    console.log(chalk.yellow('links validados'), await validaURLs(resultado));
+  } else {
+    console.log(chalk.yellow('lista de links'), resultado);
+  }
 }
 
 processaTexto(caminho);
